db05dca1c31cb9532ed9f4319d70fd51bbc47a3b
joaovitorsantoos <joaovdsfloripa@gmail.com>
2026-04-06 17:36:34 -0300

    Configurando ORM

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from cars.models import Car
 
 
 def cars_view(request):
-    return render(request, 'cars.html')
+    cars = Car.objects.all()
+
+    return render(request, 'cars.html', { 'cars': cars })
     
