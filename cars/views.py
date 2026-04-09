babf5d40382a110594d5a9bea660d974559761c8
joaovitorsantoos <joaovdsfloripa@gmail.com>
2026-04-09 12:02:47 -0300

    Filtrando pesquisa

--- cars/views.py
+++ cars/views.py
@@ -4,9 +4,15 @@ from cars.models import Car
 
 
 def cars_view(request):
-    print(request.GET)
-
     cars = Car.objects.all()
+    search = request.GET.get('search')
 
-    return render(request, 'cars.html', { 'cars': cars })
+    if search:
+        cars = Car.objects.filter(model__icontains=search).order_by('model')
+
+    return render(
+        request,
+        'cars.html',
+        {'cars': cars}
+    )
     
