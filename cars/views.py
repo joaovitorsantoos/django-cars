c6301f47627a743729dccf7e2ddd852e7beddf34
joaovitorsantoos <joaovdsfloripa@gmail.com>
2026-04-09 12:54:25 -0300

    Integrando Forms

--- cars/views.py
+++ cars/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from cars.models import Car
-
+from cars.forms import CarForm
 
 def cars_view(request):
-    cars = Car.objects.all()
+    cars = Car.objects.all().order_by('model')
     search = request.GET.get('search')
 
     if search:
-        cars = Car.objects.filter(model__icontains=search).order_by('model')
+        cars = Car.objects.filter(model__icontains=search)
 
     return render(
         request,
@@ -16,3 +16,12 @@ def cars_view(request):
         {'cars': cars}
     )
     
+def new_car_view(request):
+    new_car_form = CarForm()
+
+    return render(
+      
+        request,
+        'new_car.html',
+        {'new_car_form': new_car_form}
+    )
