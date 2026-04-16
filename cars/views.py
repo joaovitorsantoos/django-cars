f67ccf3f2606110e874cdef69ebc4759ef483de4
joaovitorsantoos <joaovdsfloripa@gmail.com>
2026-04-16 10:50:42 -0300

    Utilizando ClassBasedViews

--- cars/views.py
+++ cars/views.py
@@ -2,19 +2,24 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views import View
 
-def cars_view(request):
-    cars = Car.objects.all().order_by('model')
-    search = request.GET.get('search')
 
-    if search:
-        cars = Car.objects.filter(model__icontains=search)
+class CarsView(View):
 
-    return render(
-        request,
-        'cars.html',
-        {'cars': cars}
-    )
+    def get(self, request):
+            cars = Car.objects.all().order_by('model')
+            search = request.GET.get('search')
+
+            if search:
+                cars = Car.objects.filter(model__icontains=search)
+                
+            return render(
+                request,
+                'cars.html',
+                {'cars': cars}
+            )
+
     
 def new_car_view(request):
     if request.method == 'POST':  
