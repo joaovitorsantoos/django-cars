c3ffdba4f929bea7a8349bdc691348be03030bb8
joaovitorsantoos <joaovdsfloripa@gmail.com>
2026-04-18 10:04:11 -0300

    Utilizando ListView

--- cars/views.py
+++ cars/views.py
@@ -3,20 +3,22 @@ from django.http import HttpResponse
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
+from django.views.generic import ListView
 
 
-class CarsView(View):
+class CarsListView(ListView):
+     model = Car
+     template_name = 'cars.html'
+     context_object_name = 'cars'
 
-    def get(self, request):
-            cars = Car.objects.all().order_by('model')
-            search = request.GET.get('search')
+     def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
+        if search:
+             cars = cars.filter(model__icontains=search)
+        return cars
 
-            if search:
-                cars = Car.objects.filter(model__icontains=search)
-                
-            return render(request, 'cars.html', {'cars': cars})
-
-
+         
 class NewCarView(View):
 
     def get(self, request):
@@ -30,3 +32,6 @@ class NewCarView(View):
             new_car_form.save()
             return redirect('cars_list')
         return render(request, 'new_car.html', {'new_car_form': new_car_form})
+    
+
+     
