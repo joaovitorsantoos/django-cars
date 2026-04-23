ff60a9e478041a5c6268682697d12b6169d07b11
João Vitor <joaovdsfloripa@gmail.com>
2026-04-23 16:55:10 -0300

    DetailView

--- cars/views.py
+++ cars/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView
 
 
 class CarsListView(ListView):
@@ -17,21 +17,6 @@ class CarsListView(ListView):
         if search:
              cars = cars.filter(model__icontains=search)
         return cars
-
-         
-class NewCarView(View):
-
-    def get(self, request):
-
-        new_car_form = CarModelForm()
-        return render(request, 'new_car.html', {'new_car_form': new_car_form})
-    
-    def post(sel, request):
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-        return render(request, 'new_car.html', {'new_car_form': new_car_form})
     
 
 class NewCarCreateView(CreateView):
@@ -39,3 +24,8 @@ class NewCarCreateView(CreateView):
     form_class = CarModelForm
     template_name = 'new_car.html'
     success_url = '/cars/'
+
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
