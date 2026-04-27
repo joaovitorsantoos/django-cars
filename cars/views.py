6fc21783d67fe113657c61118fd4cff971a8eeb7
João Vitor <joaovdsfloripa@gmail.com>
2026-04-27 11:15:56 -0300

    SucessURL - UpdateView

--- cars/views.py
+++ cars/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
@@ -36,6 +37,9 @@ class CarUpdateView(UpdateView):
     form_class = CarModelForm
     template_name = 'car_update.html'
     success_url = '/cars/'
+
+    def get_success_url(self):
+        return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
 
 
 class CarDeleteView(DeleteView):
